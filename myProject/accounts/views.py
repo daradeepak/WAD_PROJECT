d2caea9b8329ab894b49d647e36b62241a71d37d
daradeepak <deepak.dara07@gmail.com>
2021-05-04 19:57:01 +0530

    updated

--- myProject/accounts/views.py
+++ myProject/accounts/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
 # Create your views here.
+
+def login(request):
+     if request.method== 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request,'invalid credentials')
+            return redirect('login')
+
+    else:
+        return render(request,'login.html')    
+
 
 def signup(request):
 
@@ -13,9 +32,10 @@ def signup(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
 
-        user = User.objects.create_user(username=username, password=password1, email=email,first_name=first_name, last_name= last_name)
+        user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
         user.save();
         print('user created')
         return redirect('/')
+
     else:
-        return render(request, 'signup.html')
+        return render(request,'signup.html')
