6419176c7c48552e6c350e617d7c8de8769eb936
daradeepak <deepak.dara07@gmail.com>
2021-05-04 20:13:36 +0530

    django

--- myProject/accounts/views.py
+++ myProject/accounts/views.py
@@ -4,24 +4,6 @@ from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
-def login(request):
-     if request.method== 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = auth.authenticate(username=username,password=password)
-
-        if user is not None:
-            auth.login(request, user)
-            return redirect("/")
-        else:
-            messages.info(request,'invalid credentials')
-            return redirect('login')
-
-    else:
-        return render(request,'login.html')    
-
-
 def signup(request):
 
     if request.method == 'POST':
